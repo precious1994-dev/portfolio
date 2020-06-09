92ce264dd59f0d769e42960583380f6926d83e17
precious1994-dev <66332559+precious1994-dev@users.noreply.github.com>
2020-06-09 02:54:51 +0200

    login

---
+++ src/Entity/Competence.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CompetenceRepository::class)
+ */
+class Competence
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * id de la personne
+     * @var [type]
+     * @ORM\ManyToOne(targetEntity="App\Entity\Personne")
+     * @ORM\JoinColumn(name="idPersonne", referencedColumnName="id")
+     */
+    private $idPersonne;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_debut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_fin;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDeDebut(): ?\DateTimeInterface
+    {
+        return $this->date_de_debut;
+    }
+
+    public function setDateDeDebut(\DateTimeInterface $date_de_debut): self
+    {
+        $this->date_de_debut = $date_de_debut;
+
+        return $this;
+    }
+
+    public function getDateDeFin(): ?\DateTimeInterface
+    {
+        return $this->date_de_fin;
+    }
+
+    public function setDateDeFin(\DateTimeInterface $date_de_fin): self
+    {
+        $this->date_de_fin = $date_de_fin;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200608233330.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200608233330 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE competence (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, date_de_debut DATE NOT NULL, date_de_fin DATE NOT NULL, note INT NOT NULL, idPersonne INT DEFAULT NULL, INDEX IDX_94D4687F5C6DE3B4 (idPersonne), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE education (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, date_de_debut DATE NOT NULL, date_de_fin DATE NOT NULL, diplome VARCHAR(255) NOT NULL, detail VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE competence ADD CONSTRAINT FK_94D4687F5C6DE3B4 FOREIGN KEY (idPersonne) REFERENCES personne (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE competence');
+        $this->addSql('DROP TABLE education');
+    }
+}
